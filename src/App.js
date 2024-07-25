425bd9909e13ac8280a9530a14f0699b8027ae2b
miss-gif <svx327@naver.com>
2024-07-25 11:03:18 +0900

    Recoil 활용

--- src/App.js
+++ src/App.js
@@ -1,45 +1,15 @@
-import { BrowserRouter, Route, RouterProvider, Routes } from "react-router-dom";
-import "./App.css";
-import Header from "./components/layout/Header";
-import Footer from "./components/layout/Footer";
+import React from "react";
+import Counter from "./pages/Counter";
+import LoginForm from "./pages/LoginForm";
 
-// Index 라는 이름 충돌로 변경함
-import Home from "./pages/Index";
-import Compony from "./pages/company/Index";
-import GoodDeatil from "./pages/good/Detail";
-import Ceo from "./pages/company/Ceo";
-import History from "./pages/company/History";
-import Partner from "./pages/company/Partner";
-import Good from "./pages/good/Good";
-import { useState } from "react";
-import router from "./router/root";
-
-function App() {
-  // 복잡한 데이터
-  const arr = [
-    { name: "삼성전자", link: "http://" },
-    { name: "LG전자", link: "http://" },
-    { name: "그린컴퓨터", link: "http://" },
-  ];
-  // 로그인 안 된 경우
-  const [isLogin, setIsLogin] = useState(true);
-
+const App = () => {
   return (
-    <>
-      {/* 공통레이아웃 적용 */}
-      <div className="wrap">
-        <Header>
-          {isLogin ? (
-            <div>정보수정/로그아웃</div>
-          ) : (
-            <div>회원가입/회원로그인</div>
-          )}
-        </Header>
-        <RouterProvider router={router} />
-        <Footer></Footer>
-      </div>
-    </>
+    <div>
+      <h1>APP 컴포넌트</h1>
+      <Counter />
+      <LoginForm />
+    </div>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { RecoilRoot } from "recoil";
 
 // ts 에서는 데이터 종류를 구별한다.
 // as 는 강제로 타입지정
@@ -11,7 +12,7 @@ import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <>
+  <RecoilRoot>
     <App />
-  </>
+  </RecoilRoot>,
 );
