f981d4527fe8e73d8aa050f6a16a7438b9454a94
miss-gif <svx327@naver.com>
2024-07-29 12:56:59 +0900

    React Quill 활용

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from "react";
 import Counter from "./pages/Counter";
 import LoginForm from "./pages/LoginForm";
 import Join from "./pages/Join";
+import Write from "./pages/Write";
 
 const App = () => {
   return (
     <div>
       <h1>APP 컴포넌트</h1>
-      <Join />
+      <Write />
       <Counter />
       <LoginForm />
     </div>
