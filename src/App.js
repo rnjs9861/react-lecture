fd78f1223fa6dc26a98a309da291ae3330b559fa
miss-gif <svx327@naver.com>
2024-07-30 11:42:40 +0900

    React Query 기본활용

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React from "react";
-import Counter from "./pages/Counter";
-import LoginForm from "./pages/LoginForm";
-import Join from "./pages/Join";
-import Write from "./pages/Write";
+import React, { useState } from "react";
+import PostList from "./pages/PostList";
+import PostAdd from "./pages/PostAdd";
+import PostUpdate from "./pages/PostUpdate";
 
 const App = () => {
+  const [item, setItem] = useState(null);
   return (
     <div>
       <h1>APP 컴포넌트</h1>
-      <Write />
-      <Counter />
-      <LoginForm />
+      <PostList setItem={setItem} />
+      <PostUpdate item={item} />
+      <PostAdd />
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { RecoilRoot } from "recoil";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 // ts 에서는 데이터 종류를 구별한다.
 // as 는 강제로 타입지정
@@ -11,8 +13,14 @@ import { RecoilRoot } from "recoil";
 // js 버전
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
+// 웹 서비스 연결 및 결과에 대한 상태관리
+const queryClient = new QueryClient();
+
 root.render(
   <RecoilRoot>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevtools initialIsOpen={true} />
+    </QueryClientProvider>
   </RecoilRoot>,
 );
