d64a7038afd24137ce2557ecfdf1b5ca4968c314
miss-gif <svx327@naver.com>
2024-08-01 10:00:51 +0900

    antdesign 기본 활용

--- src/App.js
+++ src/App.js
@@ -1,22 +1,7 @@
-import Login from "./Login";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import After from "./pages/member/After";
-import MyMap from "./kko/MyMap";
-import { useEffect } from "react";
+import JoinForm from "./pages/JoinForm";
 
 const App = () => {
-  useEffect(() => {
-    const script = document.createElement("script");
-    script.src = "https://example.com/external-script.js"; // 외부 스크립트 URL
-    script.async = true;
-    document.body.appendChild(script);
-
-    // Cleanup: 컴포넌트가 언마운트될 때 스크립트를 제거합니다.
-    return () => {
-      document.body.removeChild(script);
-    };
-  }, []);
-  return <MyMap />;
+  return <JoinForm />;
 };
 
 export default App;
