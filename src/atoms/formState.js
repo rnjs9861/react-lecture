ac535f5c7ac4536f3e13bba007b46fffdab36c3a
miss-gif <svx327@naver.com>
2024-08-05 11:56:54 +0900

    FB 기본 프로젝트 세팅 및 배포 방법

--- src/atoms/formState.js
+++
@@ -1,8 +0,0 @@
-import { atom } from "recoil";
-
-export const userNameState = atom({ key: "userNameState", default: "" });
-export const userEmailState = atom({ key: "userEmailState", default: "" });
-export const userPasswordState = atom({
-  key: "userPasswordState",
-  default: "",
-});
